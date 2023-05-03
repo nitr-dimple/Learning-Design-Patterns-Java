66d1cbe7a7cdf984f8fa150fae51be97f6c1d4c5
Dimple Patel <dimplepatel982@gmail.com>
2023-05-03 09:01:20 -0400

    Inheritance Basic

---
+++ DesignPattern/src/com/neu/dimple/inheritance/Cat.java
@@ -0,0 +1,12 @@
+package com.neu.dimple.inheritance;
+
+/**
+ * @author Dimpleben Kanjibhai Patel
+ */
+public class Cat extends Animal{
+
+    public Cat(){
+        super();
+        setSound("Meow");
+    }
+}
