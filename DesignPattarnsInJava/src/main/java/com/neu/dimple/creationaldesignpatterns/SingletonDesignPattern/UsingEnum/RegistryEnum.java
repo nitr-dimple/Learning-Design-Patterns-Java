b3fe26d5129d13c643c9bdab792424008386fa6d
Dimple Patel <dimplepate982@gmail.com>
2023-09-15 12:40:22 -0400

    Singleton Design Pattern

---
+++ DesignPattarnsInJava/src/main/java/com/neu/dimple/creationaldesignpatterns/SingletonDesignPattern/UsingEnum/RegistryEnum.java
@@ -0,0 +1,13 @@
+package com.neu.dimple.creationaldesignpatterns.SingletonDesignPattern.UsingEnum;
+
+/**
+ * @author Dimpleben Kanjibhai Patel
+ */
+public enum RegistryEnum {
+
+    INSTANCE;
+
+    public void getConfiguration(){
+        
+    }
+}
